5d09797ec2e4dee46b927e0c1ec524a7d6617480
Taufik-H <taufikrpl1@gmail.com>
2024-08-11 16:48:27 +0700

    feat:AnalyticProfider

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,19 @@
-import '@/styles/globals.css'
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import * as gtag from '../lib/gtag';
-import TagManager from 'react-gtm-module';
+import AnalyticProvider from "@/components/pagetemplate/AnalyticProvider";
+import "@/styles/globals.css";
 
 import { PagesProgressBar as ProgressBar } from "next-nprogress-bar";
 export default function App({ Component, pageProps }) {
-
-  useEffect(() => {
-    if (process.env.NEXT_PUBLIC_GTM_ID !== null) {
-      TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID });
-    }
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-  return <>
-      <Component {...pageProps} />
-      <ProgressBar
-        height="4px"
-        color="#f97316"
-        options={{ showSpinner: false }}
-        shallowRouting
-      />
+  return (
+    <>
+      <AnalyticProvider>
+        <Component {...pageProps} />
+        <ProgressBar
+          height="4px"
+          color="#f97316"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
+      </AnalyticProvider>
     </>
-
+  );
 }
