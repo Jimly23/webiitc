f2bb2138ab881da07355f99e7b1a51bd225a7031
Taufik-H <taufikrpl1@gmail.com>
2024-08-11 16:31:52 +0700

    layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
+import { PagesProgressBar as ProgressBar } from "next-nprogress-bar";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ProgressBar
+        height="4px"
+        color="#f97316"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+    </>
+  );
 }
